12e4513002d32c039e990254cb6c2931bb4c9302
madhankumar <madhankumararusamy@gmail.com>
2025-03-04 01:03:36 +0530

    Configured MySQL and created User model

--- AthletePerformanceTracker/src/main/java/com/madhan/AthletePerformanceTracker/DemoApplication.java
+++ AthletePerformanceTracker/src/main/java/com/madhan/AthletePerformanceTracker/DemoApplication.java
@@ -1,5 +1,4 @@
-package com.example.demo;
-
+package com.madhan.AthletePerformanceTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
